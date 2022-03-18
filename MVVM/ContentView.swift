72a150b15e85111f16ea6aa9e9cc685a702b25ce
Didier <did35@yahoo.com>
2022-03-18 07:38:59 +0100

    Initial Commit

---
+++ MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVM
+//
+//  Created by Didier Delhaisse on 18/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by Didier Delhaisse on 18/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
